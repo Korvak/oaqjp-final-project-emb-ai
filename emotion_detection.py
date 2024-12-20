b673fda8b72843a5f0ac34348f9b722a85841904
Virgil <virgilvancini@gmail.com>
2024-12-20 16:05:01 +0100

    created emotion_detection package

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,8 +1,50 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyse):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     jsonObj = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, json = jsonObj, headers = headers )
-    return response.text
+    formatted_response = json.loads(response.text)
+    '''this is an example response :
+    {
+        "emotionPredictions": [
+            {
+            "emotion": {
+                "anger": 0.01364663,
+                "disgust": 0.0017160787,
+                "fear": 0.008986978,
+                "joy": 0.9719017,
+                "sadness": 0.055187024
+            },
+            "target": "",
+            "emotionMentions": [
+                {
+                "span": {
+                    "begin": 0,
+                    "end": 27,
+                    "text": "I love this new technology."
+                },
+                "emotion": {
+                    "anger": 0.01364663,
+                    "disgust": 0.0017160787,
+                    "fear": 0.008986978,
+                    "joy": 0.9719017,
+                    "sadness": 0.055187024
+                }
+                }
+            ]
+            }
+        ],
+        "producerId": {
+            "name": "Ensemble Aggregated Emotion Workflow",
+            "version": "0.0.1"
+        }
+    }
+    
+    '''
+    emotions = formatted_response["emotionPredictions"][0]["emotion"]
+    emotions["dominant_emotion"] = max(emotions, key = lambda x: emotions[x])
+    #returns the emotions with the dominant one in a dictonary format emotion : normalized_score
+    return emotions
